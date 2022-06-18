64d708cbfd173c7c283dbcfd34b923aeddab8ef9
haveTryTwo <liuzhongchao197@163.com>
2022-06-18 16:36:17 +0800

    1)Adding: comments on skiplist/write_banch

--- db/skiplist.h
+++ db/skiplist.h
@@ -32,10 +32,10 @@
 
 namespace leveldb {
 
-class Arena;
+class Arena; // NOTE:htt, 提供内存分配机制,如果小内存则先缓存一部分,后续申请直接从缓存取
 
 template<typename Key, class Comparator>
-class SkipList {
+class SkipList { // NOTE:htt, 跳表
  private:
   struct Node;
 
@@ -53,7 +53,7 @@ class SkipList {
   bool Contains(const Key& key) const;
 
   // Iteration over the contents of a skip list
-  class Iterator {
+  class Iterator { // NOTE:htt, 跳表的迭代器
    public:
     // Initialize an iterator over the specified list.
     // The returned iterator is not valid.
@@ -86,31 +86,31 @@ class SkipList {
     void SeekToLast();
 
    private:
-    const SkipList* list_;
-    Node* node_;
+    const SkipList* list_; // NOTE:htt, 跳表
+    Node* node_; // NOTE:htt, 当前节点
     // Intentionally copyable
   };
 
  private:
-  enum { kMaxHeight = 12 };
+  enum { kMaxHeight = 12 }; // NOTE:htt, 最高12层
 
   // Immutable after construction
-  Comparator const compare_;
+  Comparator const compare_; // NOTE:htt, 比较器
   Arena* const arena_;    // Arena used for allocations of nodes
 
-  Node* const head_;
+  Node* const head_; // NOTE:htt, 头结点<key=0, height=12>
 
   // Modified only by Insert().  Read racily by readers, but stale
   // values are ok.
-  port::AtomicPointer max_height_;   // Height of the entire list
+  port::AtomicPointer max_height_;   // Height of the entire list // NOTE:htt, 当前最大的层高,初始化为1
 
-  inline int GetMaxHeight() const {
+  inline int GetMaxHeight() const { // NOTE:htt, 获取最高层高
     return static_cast<int>(
         reinterpret_cast<intptr_t>(max_height_.NoBarrier_Load()));
   }
 
   // Read/written only by Insert().
-  Random rnd_;
+  Random rnd_; // NOTE: htt, 随机值生成器, 注意顶多 2^31 次会轮回(算法导致)
 
   Node* NewNode(const Key& key, int height);
   int RandomHeight();
@@ -141,107 +141,107 @@ class SkipList {
 
 // Implementation details follow
 template<typename Key, class Comparator>
-struct SkipList<Key,Comparator>::Node {
+struct SkipList<Key,Comparator>::Node { // NOTE:htt, 跳表中节点
   explicit Node(const Key& k) : key(k) { }
 
-  Key const key;
+  Key const key; // NOTE:htt, 存储的key
 
   // Accessors/mutators for links.  Wrapped in methods so we can
   // add the appropriate barriers as necessary.
-  Node* Next(int n) {
+  Node* Next(int n) { // NOTE:htt, 带内存屏障获取当前节点第n层的下一个节点
     assert(n >= 0);
     // Use an 'acquire load' so that we observe a fully initialized
     // version of the returned Node.
     return reinterpret_cast<Node*>(next_[n].Acquire_Load());
   }
-  void SetNext(int n, Node* x) {
+  void SetNext(int n, Node* x) { // NOTE:htt, 带内存屏障设置当前节点第n层指向x节点
     assert(n >= 0);
     // Use a 'release store' so that anybody who reads through this
     // pointer observes a fully initialized version of the inserted node.
-    next_[n].Release_Store(x);
+    next_[n].Release_Store(x); // NOTE:htt, 带内存屏障设置 <n, x> 即当前节点第n层指向x节点
   }
 
   // No-barrier variants that can be safely used in a few locations.
-  Node* NoBarrier_Next(int n) {
+  Node* NoBarrier_Next(int n) { // NOTE:htt, 不带内存屏障方式获取当前节点第n层的下一个节点
     assert(n >= 0);
     return reinterpret_cast<Node*>(next_[n].NoBarrier_Load());
   }
-  void NoBarrier_SetNext(int n, Node* x) {
+  void NoBarrier_SetNext(int n, Node* x) { // NOTE:htt, 不带内存屏障设置当前节点第n层指向x节点
     assert(n >= 0);
     next_[n].NoBarrier_Store(x);
   }
 
  private:
   // Array of length equal to the node height.  next_[0] is lowest level link.
-  port::AtomicPointer next_[1];
+  port::AtomicPointer next_[1]; // NOTE:htt, 当前节点的层高指针数组,第0层为底层链接
 };
 
 template<typename Key, class Comparator>
 typename SkipList<Key,Comparator>::Node*
-SkipList<Key,Comparator>::NewNode(const Key& key, int height) {
+SkipList<Key,Comparator>::NewNode(const Key& key, int height) { // NOTE:htt, 分配包含key的Node,并且层高为height
   char* mem = arena_->AllocateAligned(
-      sizeof(Node) + sizeof(port::AtomicPointer) * (height - 1));
-  return new (mem) Node(key);
+      sizeof(Node) + sizeof(port::AtomicPointer) * (height - 1)); // NOTE:htt, 分配sizeof(Node)+(n-1)*指针数组空间
+  return new (mem) Node(key); // NOTE:htt, 本地分配
 }
 
 template<typename Key, class Comparator>
-inline SkipList<Key,Comparator>::Iterator::Iterator(const SkipList* list) {
+inline SkipList<Key,Comparator>::Iterator::Iterator(const SkipList* list) { // NOTE:htt, skiplist iterator初始化
   list_ = list;
   node_ = NULL;
 }
 
 template<typename Key, class Comparator>
-inline bool SkipList<Key,Comparator>::Iterator::Valid() const {
+inline bool SkipList<Key,Comparator>::Iterator::Valid() const { // NOTE:htt, 当前节点是否有效
   return node_ != NULL;
 }
 
 template<typename Key, class Comparator>
-inline const Key& SkipList<Key,Comparator>::Iterator::key() const {
+inline const Key& SkipList<Key,Comparator>::Iterator::key() const { // NOTE:htt, 获取节点key
   assert(Valid());
   return node_->key;
 }
 
 template<typename Key, class Comparator>
-inline void SkipList<Key,Comparator>::Iterator::Next() {
+inline void SkipList<Key,Comparator>::Iterator::Next() { // NOTE:htt, 下一个节点
   assert(Valid());
   node_ = node_->Next(0);
 }
 
 template<typename Key, class Comparator>
-inline void SkipList<Key,Comparator>::Iterator::Prev() {
+inline void SkipList<Key,Comparator>::Iterator::Prev() { // NOTE:htt, 获取前一个节点
   // Instead of using explicit "prev" links, we just search for the
   // last node that falls before key.
   assert(Valid());
-  node_ = list_->FindLessThan(node_->key);
+  node_ = list_->FindLessThan(node_->key); // NOTE:htt, 查找比key小的node
+  if (node_ == list_->head_) { // NOTE:htt, 已到头部
+    node_ = NULL;
+  }
+}
+
+template<typename Key, class Comparator>
+inline void SkipList<Key,Comparator>::Iterator::Seek(const Key& target) { // NOTE:htt, 查找和key相等或大于的node
+  node_ = list_->FindGreaterOrEqual(target, NULL); // NOTE:htt, 查找和key相等或大于的node
+}
+
+template<typename Key, class Comparator>
+inline void SkipList<Key,Comparator>::Iterator::SeekToFirst() { // NOTE:htt, 指向跳表中第一个node
+  node_ = list_->head_->Next(0); // NOTE:htt, 指向第一个node
+}
+
+template<typename Key, class Comparator>
+inline void SkipList<Key,Comparator>::Iterator::SeekToLast() { // NOTE:htt, 指向跳表中最后一个node
+  node_ = list_->FindLast(); // NOTE:htt, 找到最后一个node
   if (node_ == list_->head_) {
     node_ = NULL;
   }
 }
 
 template<typename Key, class Comparator>
-inline void SkipList<Key,Comparator>::Iterator::Seek(const Key& target) {
-  node_ = list_->FindGreaterOrEqual(target, NULL);
-}
-
-template<typename Key, class Comparator>
-inline void SkipList<Key,Comparator>::Iterator::SeekToFirst() {
-  node_ = list_->head_->Next(0);
-}
-
-template<typename Key, class Comparator>
-inline void SkipList<Key,Comparator>::Iterator::SeekToLast() {
-  node_ = list_->FindLast();
-  if (node_ == list_->head_) {
-    node_ = NULL;
-  }
-}
-
-template<typename Key, class Comparator>
-int SkipList<Key,Comparator>::RandomHeight() {
+int SkipList<Key,Comparator>::RandomHeight() { // NOTE:htt, 获取随机height
   // Increase height with probability 1 in kBranching
   static const unsigned int kBranching = 4;
   int height = 1;
-  while (height < kMaxHeight && ((rnd_.Next() % kBranching) == 0)) {
+  while (height < kMaxHeight && ((rnd_.Next() % kBranching) == 0)) { // NOTE:htt, 1/4概率会增加height
     height++;
   }
   assert(height > 0);
@@ -250,85 +250,45 @@ int SkipList<Key,Comparator>::RandomHeight() {
 }
 
 template<typename Key, class Comparator>
-bool SkipList<Key,Comparator>::KeyIsAfterNode(const Key& key, Node* n) const {
+bool SkipList<Key,Comparator>::KeyIsAfterNode(const Key& key, Node* n) const { // NOTE:htt, 判断n->key是否比key小
   // NULL n is considered infinite
   return (n != NULL) && (compare_(n->key, key) < 0);
 }
 
 template<typename Key, class Comparator>
 typename SkipList<Key,Comparator>::Node* SkipList<Key,Comparator>::FindGreaterOrEqual(const Key& key, Node** prev)
-const {/*{{{*/
+const { // NOTE:htt, 查找和当前key大或相等的节点 /*{{{*/
   Node* x = head_;
-  int level = GetMaxHeight() - 1;
+  int level = GetMaxHeight() - 1; // NOTE:htt, 获取当前跳表的最高height
   while (true) {
     Node* next = x->Next(level);
 
-    if (next != NULL) {/*{{{*/
-        uint32_t len;
-        const char* p = next->key;
-        p = GetVarint32Ptr(p, p + 5, &len);  // +5: we assume "p" is not corrupted
-        std::string next_key(p, len);
-
-        fprintf(stderr, "next key:");
-        for (size_t i = 0; i < len; ++i) {
-            fprintf(stderr, "%02x", (uint8_t)p[i]);
-        }
-
-        fprintf(stderr, "  key:");
-        p = key;
-        p = GetVarint32Ptr(p, p + 5, &len);  // +5: we assume "p" is not corrupted
-        for (size_t i = 0; i < len; ++i) {
-            fprintf(stderr, "%02x", (uint8_t)p[i]);
-        }
-        fprintf(stderr, "\n");
-    }/*}}}*/
-
-    if (KeyIsAfterNode(key, next)) {
+//    if (next != NULL) {/*{{{*/
+//        uint32_t len;
+//        const char* p = next->key;
+//        p = GetVarint32Ptr(p, p + 5, &len);  // +5: we assume "p" is not corrupted // NOTE:htt, 获取跳表中key的长度
+//        std::string next_key(p, len); // NOTE:htt, 获取key
+//
+//        fprintf(stderr, "next key:");
+//        for (size_t i = 0; i < len; ++i) {
+//            fprintf(stderr, "%02x", (uint8_t)p[i]);
+//        }
+//
+//        fprintf(stderr, "  key:");
+//        p = key;
+//        p = GetVarint32Ptr(p, p + 5, &len);  // +5: we assume "p" is not corrupted // NOTE:htt, 获取key的长度
+//        for (size_t i = 0; i < len; ++i) {
+//            fprintf(stderr, "%02x", (uint8_t)p[i]);
+//        }
+//        fprintf(stderr, "\n");
+//    }/*}}}*/
+    if (KeyIsAfterNode(key, next)) { // NOTE:htt, key在node之后
       // Keep searching in this list
       x = next;
-        if (next != NULL) {/*{{{*/
-            uint32_t len;
-            const char* p = next->key;
-            p = GetVarint32Ptr(p, p + 5, &len);  // +5: we assume "p" is not corrupted
-            std::string next_key(p, len);
-
-            fprintf(stderr, "less next key:");
-            for (size_t i = 0; i < len; ++i) {
-                fprintf(stderr, "%02x", (uint8_t)p[i]);
-            }
-
-            fprintf(stderr, "  key:");
-            p = key;
-            p = GetVarint32Ptr(p, p + 5, &len);  // +5: we assume "p" is not corrupted
-            for (size_t i = 0; i < len; ++i) {
-                fprintf(stderr, "%02x", (uint8_t)p[i]);
-            }
-            fprintf(stderr, "\n");
-        }/*}}}*/
-    } else {
+    } else { // NOTE:htt, key在node之前
       if (prev != NULL) prev[level] = x;
-      if (level == 0) {
-        if (next != NULL) {/*{{{*/
-            uint32_t len;
-            const char* p = next->key;
-            p = GetVarint32Ptr(p, p + 5, &len);  // +5: we assume "p" is not corrupted
-            std::string next_key(p, len);
-
-            fprintf(stderr, "finish next key:");
-            for (size_t i = 0; i < len; ++i) {
-                fprintf(stderr, "%02x", (uint8_t)p[i]);
-            }
-
-            fprintf(stderr, "  key:");
-            p = key;
-            p = GetVarint32Ptr(p, p + 5, &len);  // +5: we assume "p" is not corrupted
-            for (size_t i = 0; i < len; ++i) {
-                fprintf(stderr, "%02x", (uint8_t)p[i]);
-            }
-            fprintf(stderr, "\n");
-        }/*}}}*/
-
-        return next;
+      if (level == 0) { // NOTE:htt, 到底
+        return next; // NOTE:htt, 返回下一个node,和key相等或大于key
       } else {
         // Switch to next list
         level--;
@@ -339,20 +299,20 @@ const {/*{{{*/
 
 template<typename Key, class Comparator>
 typename SkipList<Key,Comparator>::Node*
-SkipList<Key,Comparator>::FindLessThan(const Key& key) const {/*{{{*/
+SkipList<Key,Comparator>::FindLessThan(const Key& key) const { // NOTE:htt, 找比key小的node /*{{{*/
   Node* x = head_;
-  int level = GetMaxHeight() - 1;
+  int level = GetMaxHeight() - 1;// NOTE:htt, 获取当前跳表的最高height
   while (true) {
     assert(x == head_ || compare_(x->key, key) < 0);
     Node* next = x->Next(level);
-    if (next == NULL || compare_(next->key, key) >= 0) {
-      if (level == 0) {
+    if (next == NULL || compare_(next->key, key) >= 0) { // NOTE:htt, key在next node前
+      if (level == 0) { // NOTE:htt, 到底
         return x;
       } else {
         // Switch to next list
         level--;
       }
-    } else {
+    } else { // NOTE:htt, key在next node之后
       x = next;
     }
   }
@@ -360,13 +320,13 @@ SkipList<Key,Comparator>::FindLessThan(const Key& key) const {/*{{{*/
 
 template<typename Key, class Comparator>
 typename SkipList<Key,Comparator>::Node* SkipList<Key,Comparator>::FindLast()
-    const {/*{{{*/
+    const { // NOTE:htt, 查找最后一个node /*{{{*/
   Node* x = head_;
-  int level = GetMaxHeight() - 1;
+  int level = GetMaxHeight() - 1; // NOTE:htt, 获取当前跳表的最高height
   while (true) {
     Node* next = x->Next(level);
     if (next == NULL) {
-      if (level == 0) {
+      if (level == 0) { // NOTE:htt, 到底
         return x;
       } else {
         // Switch to next list
@@ -382,28 +342,28 @@ template<typename Key, class Comparator>
 SkipList<Key,Comparator>::SkipList(Comparator cmp, Arena* arena)
     : compare_(cmp),
       arena_(arena),
-      head_(NewNode(0 /* any key will do */, kMaxHeight)),
+      head_(NewNode(0 /* any key will do */, kMaxHeight)), // NOTE:htt, 头结点<key=0, height=12>
       max_height_(reinterpret_cast<void*>(1)),
       rnd_(0xdeadbeef) {/*{{{*/
   for (int i = 0; i < kMaxHeight; i++) {
-    head_->SetNext(i, NULL);
+    head_->SetNext(i, NULL); // NOTE:htt, 头节点每层的指向为NULL
   }
 }/*}}}*/
 
 template<typename Key, class Comparator>
-void SkipList<Key,Comparator>::Insert(const Key& key) {/*{{{*/
+void SkipList<Key,Comparator>::Insert(const Key& key) { // NOTE:htt, 查询新的key /*{{{*/
   // TODO(opt): We can use a barrier-free variant of FindGreaterOrEqual()
   // here since Insert() is externally synchronized.
   Node* prev[kMaxHeight];
-  Node* x = FindGreaterOrEqual(key, prev);
+  Node* x = FindGreaterOrEqual(key, prev); // NOTE:htt, 查找和当前key相同或大于的node
 
   // Our data structure does not allow duplicate insertion
-  assert(x == NULL || !Equal(key, x->key));
+  assert(x == NULL || !Equal(key, x->key)); // NOTE:htt, 不允许相同key插入
 
-  int height = RandomHeight();
+  int height = RandomHeight(); // NOTE:htt, 获取随机height
   if (height > GetMaxHeight()) {
     for (int i = GetMaxHeight(); i < height; i++) {
-      prev[i] = head_;
+      prev[i] = head_; // NOTE:htt, 调整[current max_height_, height]层的指针为 head_
     }
     //fprintf(stderr, "Change height from %d to %d\n", max_height_, height);
 
@@ -414,22 +374,22 @@ void SkipList<Key,Comparator>::Insert(const Key& key) {/*{{{*/
     // the loop below.  In the former case the reader will
     // immediately drop to the next level since NULL sorts after all
     // keys.  In the latter case the reader will use the new node.
-    max_height_.NoBarrier_Store(reinterpret_cast<void*>(height));
+    max_height_.NoBarrier_Store(reinterpret_cast<void*>(height)); // NOTE:htt, 设置新的层高
   }
 
   x = NewNode(key, height);
   for (int i = 0; i < height; i++) {
     // NoBarrier_SetNext() suffices since we will add a barrier when
     // we publish a pointer to "x" in prev[i].
-    x->NoBarrier_SetNext(i, prev[i]->NoBarrier_Next(i));
-    prev[i]->SetNext(i, x);
+    x->NoBarrier_SetNext(i, prev[i]->NoBarrier_Next(i)); // NOTE:htt, 调整新node的i层指向prev[i]节点i的对应节点
+    prev[i]->SetNext(i, x); // NOTE:htt, prev[i]节点指向新节点
   }
 }/*}}}*/
 
 template<typename Key, class Comparator>
-bool SkipList<Key,Comparator>::Contains(const Key& key) const {/*{{{*/
+bool SkipList<Key,Comparator>::Contains(const Key& key) const { // NOTE:htt, 判断是否包含key /*{{{*/
   Node* x = FindGreaterOrEqual(key, NULL);
-  if (x != NULL && Equal(key, x->key)) {
+  if (x != NULL && Equal(key, x->key)) { // NOTE:htt, 相等则返回true
     return true;
   } else {
     return false;
--- db/write_batch.cc
+++ db/write_batch.cc
@@ -34,36 +34,36 @@ WriteBatch::~WriteBatch() { }
 
 WriteBatch::Handler::~Handler() { }
 
-void WriteBatch::Clear() {
+void WriteBatch::Clear() { // NOTE:htt, 清空rep
   rep_.clear();
-  rep_.resize(kHeader);
+  rep_.resize(kHeader); // NOTE:htt, 设置头部长度: 序号+count
 }
 
-Status WriteBatch::Iterate(Handler* handler) const {
+Status WriteBatch::Iterate(Handler* handler) const { // NOTE:htt, 遍历rep中的<key,value>逐条写入Memtable,根据操作类型Put()或Delete()
   Slice input(rep_);
   if (input.size() < kHeader) {
     return Status::Corruption("malformed WriteBatch (too small)");
   }
 
-  input.remove_prefix(kHeader);
+  input.remove_prefix(kHeader); // NOTE:htt, input跳过头部
   Slice key, value;
   int found = 0;
-  while (!input.empty()) {
+  while (!input.empty()) { // NOTE:htt, 遍历input字符串中 <key,value>
     found++;
-    char tag = input[0];
+    char tag = input[0]; // NOTE:htt, 操作类型
     input.remove_prefix(1);
     switch (tag) {
-      case kTypeValue:
+      case kTypeValue: // NOTE;htt,写入类型则插入<key,value>
         if (GetLengthPrefixedSlice(&input, &key) &&
             GetLengthPrefixedSlice(&input, &value)) {
-          handler->Put(key, value);
+          handler->Put(key, value); // NOTE:htt, 执行写入
         } else {
           return Status::Corruption("bad WriteBatch Put");
         }
         break;
-      case kTypeDeletion:
+      case kTypeDeletion: // NOTE;htt, 删除类型则删除数据
         if (GetLengthPrefixedSlice(&input, &key)) {
-          handler->Delete(key);
+          handler->Delete(key); // NOTE:htt, 执行删除
         } else {
           return Status::Corruption("bad WriteBatch Delete");
         }
@@ -72,76 +72,76 @@ Status WriteBatch::Iterate(Handler* handler) const {
         return Status::Corruption("unknown WriteBatch tag");
     }
   }
-  if (found != WriteBatchInternal::Count(this)) {
+  if (found != WriteBatchInternal::Count(this)) { // NOTE:htt, 判断遍历得到的统计和缓存的中统计是否一致
     return Status::Corruption("WriteBatch has wrong count");
   } else {
     return Status::OK();
   }
 }
 
-int WriteBatchInternal::Count(const WriteBatch* b) {
+int WriteBatchInternal::Count(const WriteBatch* b) { // NOTE:htt, 获取rep缓存的记录个数
   return DecodeFixed32(b->rep_.data() + 8);
 }
 
-void WriteBatchInternal::SetCount(WriteBatch* b, int n) {
+void WriteBatchInternal::SetCount(WriteBatch* b, int n) { // NOTE:htt, 设置rep的缓存的记录个数
   EncodeFixed32(&b->rep_[8], n);
 }
 
-SequenceNumber WriteBatchInternal::Sequence(const WriteBatch* b) {
+SequenceNumber WriteBatchInternal::Sequence(const WriteBatch* b) { // NOTE:htt, 获取rep缓存的sequence number
   return SequenceNumber(DecodeFixed64(b->rep_.data()));
 }
 
-void WriteBatchInternal::SetSequence(WriteBatch* b, SequenceNumber seq) {
+void WriteBatchInternal::SetSequence(WriteBatch* b, SequenceNumber seq) { // NOTE:htt, 设置rep缓存的sequence number
   EncodeFixed64(&b->rep_[0], seq);
 }
 
-void WriteBatch::Put(const Slice& key, const Slice& value) {
-  WriteBatchInternal::SetCount(this, WriteBatchInternal::Count(this) + 1);
-  rep_.push_back(static_cast<char>(kTypeValue));
-  PutLengthPrefixedSlice(&rep_, key);
-  PutLengthPrefixedSlice(&rep_, value);
+void WriteBatch::Put(const Slice& key, const Slice& value) { // NOTE:htt, 添加<key,value> 到rep_
+  WriteBatchInternal::SetCount(this, WriteBatchInternal::Count(this) + 1); // NOTE:htt, 增加记录count
+  rep_.push_back(static_cast<char>(kTypeValue)); // NOTE:htt, 添加增加的类型
+  PutLengthPrefixedSlice(&rep_, key); // NOTE:htt,保存长度+字符串(key),其中长度采用可变整数
+  PutLengthPrefixedSlice(&rep_, value);// NOTE:htt,保存长度+字符串(value),其中长度采用可变整数
 }
 
-void WriteBatch::Delete(const Slice& key) {
-  WriteBatchInternal::SetCount(this, WriteBatchInternal::Count(this) + 1);
-  rep_.push_back(static_cast<char>(kTypeDeletion));
-  PutLengthPrefixedSlice(&rep_, key);
+void WriteBatch::Delete(const Slice& key) { // NOTE:htt, 添加删除key 到rep_
+  WriteBatchInternal::SetCount(this, WriteBatchInternal::Count(this) + 1); // NOTE:htt, 增加记录count
+  rep_.push_back(static_cast<char>(kTypeDeletion)); // NOTE:htt, 设置删除类型
+  PutLengthPrefixedSlice(&rep_, key); // NOTE:htt,保存长度+字符串(key),其中长度采用可变整数
 }
 
 namespace {
-class MemTableInserter : public WriteBatch::Handler {
+class MemTableInserter : public WriteBatch::Handler { // NOTE:htt, 内存Memtable写入
  public:
-  SequenceNumber sequence_;
+  SequenceNumber sequence_; // NOTE:htt, 当前seq值,每插入记录都会递增
   MemTable* mem_;
 
   virtual void Put(const Slice& key, const Slice& value) {
-    mem_->Add(sequence_, kTypeValue, key, value);
+    mem_->Add(sequence_, kTypeValue, key, value); // NOTE:htt, 跳表中插入{{key,seq,t}, value}
     sequence_++;
   }
   virtual void Delete(const Slice& key) {
-    mem_->Add(sequence_, kTypeDeletion, key, Slice());
+    mem_->Add(sequence_, kTypeDeletion, key, Slice()); // NOTE:htt, 跳表中插入删除{{key,seq,t}, {}}
     sequence_++;
   }
 };
 }  // namespace
 
 Status WriteBatchInternal::InsertInto(const WriteBatch* b,
-                                      MemTable* memtable) {
+                                      MemTable* memtable) { // NOTE:htt, 将WriteBatch中记录逐条写入到MemTable中
   MemTableInserter inserter;
-  inserter.sequence_ = WriteBatchInternal::Sequence(b);
+  inserter.sequence_ = WriteBatchInternal::Sequence(b); // NOTE:htt, 获取rep缓存的sequence number
   inserter.mem_ = memtable;
-  return b->Iterate(&inserter);
+  return b->Iterate(&inserter); // NOTE:htt, 将WriteBatch中值写入到Memtable中,写入时每条记录会增加seq
 }
 
-void WriteBatchInternal::SetContents(WriteBatch* b, const Slice& contents) {
+void WriteBatchInternal::SetContents(WriteBatch* b, const Slice& contents) { // NOTE:htt, 重新设置WriteBatch.rep_为contents,格式为 ${seq}${count}[{${type}${key}${value}]...
   assert(contents.size() >= kHeader);
   b->rep_.assign(contents.data(), contents.size());
 }
 
-void WriteBatchInternal::Append(WriteBatch* dst, const WriteBatch* src) {
-  SetCount(dst, Count(dst) + Count(src));
+void WriteBatchInternal::Append(WriteBatch* dst, const WriteBatch* src) { // NOTE:htt, 将src记录追加到dst中
+  SetCount(dst, Count(dst) + Count(src)); // NOTE:htt, 设置记录条数为 count(dst)+count(src)
   assert(src->rep_.size() >= kHeader);
-  dst->rep_.append(src->rep_.data() + kHeader, src->rep_.size() - kHeader);
+  dst->rep_.append(src->rep_.data() + kHeader, src->rep_.size() - kHeader); // NOTE:htt, 将src记录追加到dst中
 }
 
 }  // namespace leveldb
