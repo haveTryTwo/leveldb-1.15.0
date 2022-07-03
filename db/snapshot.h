fa28613a1a86b7138dd6274c2e8bc429dbc9290c
haveTryTwo <liuzhongchao197@163.com>
2022-07-03 23:13:21 +0800

    1)Adding: comments on snapshot

--- db/snapshot.h
+++ db/snapshot.h
@@ -13,23 +13,23 @@ class SnapshotList;
 
 // Snapshots are kept in a doubly-linked list in the DB.
 // Each SnapshotImpl corresponds to a particular sequence number.
-class SnapshotImpl : public Snapshot {
+class SnapshotImpl : public Snapshot { // NOTE:htt, 具体的快照,保存SequenceNumber,并通过next_/prev_将对象串联起来
  public:
-  SequenceNumber number_;  // const after creation
+  SequenceNumber number_;  // const after creation // NOTE:htt, 当前seqNumber
 
  private:
   friend class SnapshotList;
 
   // SnapshotImpl is kept in a doubly-linked circular list
-  SnapshotImpl* prev_;
-  SnapshotImpl* next_;
+  SnapshotImpl* prev_; // NOTE:htt, prev快照
+  SnapshotImpl* next_; // NOTE:htt, next快照
 
-  SnapshotList* list_;                 // just for sanity checks
+  SnapshotList* list_;                 // just for sanity checks // NOTE:htt, 保存当前的snapshotList对象,用于验证
 };
 
-class SnapshotList {
+class SnapshotList { // NOTE:htt, snaphost链表,将snapshot串联起来
  public:
-  SnapshotList() {
+  SnapshotList() { // NOTE:htt, 快照初始化指向自己
     list_.prev_ = &list_;
     list_.next_ = &list_;
   }
@@ -38,10 +38,10 @@ class SnapshotList {
   SnapshotImpl* oldest() const { assert(!empty()); return list_.next_; }
   SnapshotImpl* newest() const { assert(!empty()); return list_.prev_; }
 
-  const SnapshotImpl* New(SequenceNumber seq) {
+  const SnapshotImpl* New(SequenceNumber seq) { // NOTE:htt,插入新节点,其中new node的为list.prev_,原有新节点的next_
     SnapshotImpl* s = new SnapshotImpl;
     s->number_ = seq;
-    s->list_ = this;
+    s->list_ = this; // NOTE:htt, 保存SnapshotList对象到当前的snaphost中
     s->next_ = &list_;
     s->prev_ = list_.prev_;
     s->prev_->next_ = s;
@@ -49,8 +49,8 @@ class SnapshotList {
     return s;
   }
 
-  void Delete(const SnapshotImpl* s) {
-    assert(s->list_ == this);
+  void Delete(const SnapshotImpl* s) { // NOTE:htt, 删除当前快照列表中一个快照
+    assert(s->list_ == this); // NOTE:htt, 必须是当前的snapshotList中的快照,才允许删除
     s->prev_->next_ = s->next_;
     s->next_->prev_ = s->prev_;
     delete s;
